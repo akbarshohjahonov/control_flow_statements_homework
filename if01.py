08cad988798cb8f4a994b8d895263f3f9443aeb5
akbarshohjahonov <akbarshohjahonov@akbarshohjahonov@gmail.com>
2023-01-03 17:57:24 +0500

    kskk

--- if01.py
+++ if01.py
@@ -1,9 +1,12 @@
 def main(a):
     """
+
     If the number is positive, increase it to 1, else leave unchanged.
     Args:
         a: integer
     Returns:
         a: a increased by 1 if positive, else unchanged.
     """
-    return
+    if a>0:
+        return a+1
+print(main(5))
